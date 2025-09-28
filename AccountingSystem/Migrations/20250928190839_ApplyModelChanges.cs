486f99e5bac1563e90155ed6605035d14bf3d6a7
aymandaqa <aymandaqah@gmail.com>
2025-09-28 22:23:52 +0300

    Add migration removing AssetType account uniqueness

---
+++ AccountingSystem/Migrations/20250928190839_ApplyModelChanges.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AssetTypes_AccountId",
+                table: "AssetTypes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetTypes_AccountId",
+                table: "AssetTypes",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AssetTypes_AccountId",
+                table: "AssetTypes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetTypes_AccountId",
+                table: "AssetTypes",
+                column: "AccountId",
+                unique: true);
+        }
+    }
+}
