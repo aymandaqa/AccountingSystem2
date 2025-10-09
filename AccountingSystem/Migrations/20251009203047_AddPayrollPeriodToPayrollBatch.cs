cd77b59324710daec8833a6fbd48a6182491d231
aymandaqa <aymandaqah@gmail.com>
2025-10-10 00:02:43 +0300

    Add payroll period selection to salary processing

---
+++ AccountingSystem/Migrations/20251009203047_AddPayrollPeriodToPayrollBatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPayrollPeriodToPayrollBatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Month",
+                table: "PayrollBatches",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "PayrollBatches",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Month",
+                table: "PayrollBatches");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "PayrollBatches");
+        }
+    }
+}
