9f5825a0cd6838ff59c2a8a9738016c9ff17288e
aymandaqa <aymandaqah@gmail.com>
2025-10-18 15:07:04 +0300

    Allow null supplier on payment vouchers

---
+++ AccountingSystem/Migrations/20260116090000_AllowNullSupplierInPaymentVouchers.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingSystem.Migrations
+{
+    public partial class AllowNullSupplierInPaymentVouchers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentVouchers_Suppliers_SupplierId",
+                table: "PaymentVouchers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "PaymentVouchers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentVouchers_Suppliers_SupplierId",
+                table: "PaymentVouchers",
+                column: "SupplierId",
+                principalTable: "Suppliers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentVouchers_Suppliers_SupplierId",
+                table: "PaymentVouchers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "PaymentVouchers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentVouchers_Suppliers_SupplierId",
+                table: "PaymentVouchers",
+                column: "SupplierId",
+                principalTable: "Suppliers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
