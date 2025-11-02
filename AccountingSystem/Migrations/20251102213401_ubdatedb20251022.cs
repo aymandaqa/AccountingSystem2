1aa861576525df22aa115037b1a0bbb92964b29a
Ayman Daqa <aymandaqa@hotmail.com>
2025-11-02 23:40:35 +0200

    fix

---
+++ AccountingSystem/Migrations/20251102213401_ubdatedb20251022.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ubdatedb20251022 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorizedOperations",
+                table: "Suppliers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorizedOperations",
+                table: "Suppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 3,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
